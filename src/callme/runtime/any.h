4e96b0605d65444c8e966f868886713f7c427b0c
chenshenchao <chenshenchao@outlook.com>
2023-10-18 02:39:10 +0800

    callme.

---
+++ src/callme/runtime/any.h
@@ -0,0 +1,6 @@
+#ifndef CALLME_RUNTIME_ANY_H_INCLUDED
+#define CALLME_RUNTIME_ANY_H_INCLUDED
+
+
+
+#endif
--- src/callme/token.h
+++ src/callme/token.h
@@ -3,16 +3,25 @@
 
 namespace callme {
     enum token {
-        eof = -1,
-        literal_null,
-        literal_true,
-        literal_false,
-        literal_number,
-        literal_string,
+        eof = -1,       // 文件结尾
+        literal_null,   // null
+        literal_true,   // true
+        literal_false,  // false
+        literal_number, // 数字字面量
+        literal_string, // 字符串字面量
 
-        symbol_,
+        symbol_question, // ?
+        symbol_dot, // .
+        symbol_arrow, // ->
 
-        identifier,
+        identifier, // 标识符
+
+        keyword_return, // 返回
+        keyword_import, // 导入
+        keyword_export, // 导出
+        keyword_var, // var 可变量
+        keyword_val, // val 不变量
+
     };
 }
 
